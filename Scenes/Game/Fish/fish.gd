371aca7758c6f33da2ea39e73c0ff706691b3ebb
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-01-25 11:58:41 +1300

    new fish will respawn on leave

--- Scenes/Game/Fish/fish.gd
+++ Scenes/Game/Fish/fish.gd
@@ -5,6 +5,7 @@ class_name Fish
 signal display_order(item_name: String)
 signal place_order()
 signal pay(amount: int)
+signal left_shop()
 
 @onready var sprite = $Sprite2D
 
@@ -24,6 +25,7 @@ enum Type { ## type of fish, turtle??
 var type: Type
 var is_adult: bool
 var order: Order
+var drink: String #Drink
 var satisfaction: int = 100  # 0-100
 #var state: State = State.ENTERING
 
@@ -36,14 +38,41 @@ func _ready():
 	print(order.PrintOut())
 
 
-
+# display the order in UI and place the order to the shop
 func request_order():
 	display_order.emit(order.PrintOut())
 	place_order.emit()
 
+# receive the drink from the shop
+func receive_drink(drink: String):
+	self.drink = drink
+	print("Received drink: %s" % drink)
+	make_payment()
+
+# calculate the satisfaction based on the drink
+func calculate_satisfaction():
+	if drink == order.PrintOut():       										#######"need to compare the drink and the order"
+		satisfaction = 100
+	else:
+		satisfaction = 0                                                        ########### too harsh
+	print("Satisfaction: %d" % satisfaction)
+
+# leave the shop
+func leave_shop():
+	left_shop.emit()
+	queue_free()
+
+# pay the order
+"to global? shop?"
 func make_payment():
 	pay.emit(satisfaction)
 
+
+
+
+
+
+# print out the fish info for debugging
 func get_info() -> String:
 	return "Type: %s, Adult: %s, Satisfaction: %d, Order: %s" % [
 		Type.keys()[type],
--- Scenes/Game/Fish/fish_spawner.gd
+++ Scenes/Game/Fish/fish_spawner.gd
@@ -3,23 +3,34 @@ extends Node2D
 
 
 @export var fish_scene: PackedScene
+@export var fish_spawn_interval: float = 0.5
 var current_fish: Fish = null
 
 func spawn_fish():
-	"if current_fish != null:
-		return"
+	if current_fish != null:
+		return
 		
 	var fish = fish_scene.instantiate()
 	fish.type = Fish.Type.values()[randi() % Fish.Type.size()]
 	fish.is_adult = randf() > 0.2
 	add_child(fish)
 	current_fish = fish
+	fish.left_shop.connect(_on_Fish_left)
 
 	# Connect to fish exit signal
 	fish.tree_exiting.connect(func(): current_fish = null)
 	print("Fish spawned")
 	print(fish.get_info())
 
+# fish left
+func _on_Fish_left():
+	current_fish = null
+	await get_tree().create_timer(fish_spawn_interval).timeout
+	#print("Fish left")
+	spawn_fish()
+
+
 
 func _on_button_pressed():
 	spawn_fish()
+
