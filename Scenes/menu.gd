104ee97bce038d94eea7f664b08089a1e285d807
BBBrezel <erin.thomasriley@gmail.com>
2025-01-25 08:36:17 +1300

    minor button fix

--- Scenes/menu.gd
+++ Scenes/menu.gd
@@ -6,10 +6,11 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func _on_start_btn_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Game/MainGame.tscn")
+	GameManager.curr_game_state = GameManager.GameStates.GAME
+	get_tree().change_scene_to_file("res://Scenes/Game/Main_Game.tscn")
 
 func _on_settings_btn_pressed() -> void:
-	#settings is currently a pop up, can easily be changed to a seperate scene 
+	#settings is currently a pop up, can easily be changed to a seperate screen
 	var sett = load("res://Scenes/Settings.tscn").instantiate();
 	get_tree().current_scene.add_child(sett)
 
