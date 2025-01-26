7a2c218f9e5640487fb25eb123b9da4bce58618f
BBBrezel <erin.thomasriley@gmail.com>
2025-01-26 14:11:52 +1300

    endscreen setup

---
+++ Scenes/game_end.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+func _on_next_btn_pressed() -> void:
+	$NextBtn.hide()
+	$Nobubblestextbox.show()
+	$Label.show()
+	$ReturnBtn.show()
+
+
+func _on_return_btn_pressed() -> void:
+	GameManager.curr_game_state = GameManager.GameStates.MENU
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
